d5e90763e1753460234b3c2cc2ca62be4927c78a
Thomas Berger <thomas.berger@epfedu.fr>
2022-10-25 16:48:08 +0200

    TP1 Partie 3 Ajout du Switch

--- TP1_GuessMyNumber_BERGER/src/tp1_guessmynumber_berger/TP1_GuessMyNumber_BERGER.java
+++ TP1_GuessMyNumber_BERGER/src/tp1_guessmynumber_berger/TP1_GuessMyNumber_BERGER.java
@@ -20,25 +20,58 @@ public class TP1_GuessMyNumber_BERGER {
      */
     public static void main(String[] args) {
         
-        Random generateurAlea = new Random(100);
+        Random generateurAlea = new Random();
         double rand = generateurAlea.nextInt(100);
         Scanner sc = new Scanner (System.in);
         double saisi = -1;
+        int nbrT = 0;
+        
+        int mode = selectmode();
+        
+        switch(mode){
+            
+            case 1: //Facile
+            case 2: //Moyen 
+            case 3: //Difficile
+            case 4: //Cauchemard
+        }
         
         while(!(saisi == rand)){
             
             saisi = sc.nextInt();
-            
+            nbrT += 1; 
             if(saisi < rand){
-                System.out.println("Trop petit!");
+                System.out.println("Trop petit! \nNombre de tentatives : "+nbrT);
             }else if(saisi > rand){
-                System.out.println("Trop grand!");
+                System.out.println("Trop grand! \nNombre de tentatives : "+nbrT);
             }else{
-                System.out.println("Bravo ! Gagné ! Le numéro était bien "+rand);
+                System.out.println("Bravo ! Gagné ! Le numéro était bien "+rand +"\nNombre de tentatives : "+nbrT);
                 break;
             }
             
         } 
+        
+    }
+     private static int selectmode(){
+
+        int mode = 0;
+        Scanner sc;
+        sc = new Scanner(System.in);
+
+        System.out.println("""
+                           Selectionnez la difficulté : 
+                           1 -> Facile
+                           2 -> Moyen
+                           3 -> Difficile
+                           4 -> Cauchemard""");
+
+        while(mode<1 | mode>4){
+
+            mode = sc.nextInt();
+
+        }
+
+        return mode;
         
     }
     
