124eb2b0f4e20357aab3d8c986ac2342af20d9c0
Thomas Berger <thomas.berger@epfedu.fr>
2022-10-28 16:53:39 +0200

    TP2 - Partie 4 (et pas 3..) Fin

--- TP2_relation_1_BERGER/src/tp2_relation_1_berger/TP2_relation_1_BERGER.java
+++ TP2_relation_1_BERGER/src/tp2_relation_1_berger/TP2_relation_1_BERGER.java
@@ -19,16 +19,33 @@ public class TP2_relation_1_BERGER {
         Voiture uneAutreClio = new Voiture ("Clio", "Renault", 5 ) ;
         Voiture une2008 = new Voiture ("2008", "Peugeot", 6 ) ;
         Voiture uneMicra = new Voiture ("Micra", "Nissan", 4 ) ;
+        Voiture uneGolf = new Voiture("Golf","Volfswagen",115);
         Personne bob = new Personne("Bobby", "Sixkiller");
         Personne reno = new Personne("Reno", "Raines");
         System.out.println("liste des voitures disponibles :\n"+ uneClio + "\n" + uneAutreClio + "\n" + une2008 + "\n" + uneMicra ) ;
 
         bob.liste_voitures[0] = uneClio ;
         bob.nbVoitures = 1 ;
-        uneClio.proprietaire = Bob ;
+        uneClio.proprietaire = bob ;
         
-        System.out.println("La premiere voiture de Bob est "+ bob.liste_voiture[0]);
+        System.out.println("La premiere voiture de Bob est "+ bob.liste_voitures[0]);
         
+        bob.liste_voitures[1] = une2008 ;
+        bob.nbVoitures = 2 ;
+        une2008.proprietaire = bob ;
+        
+        reno.liste_voitures[0] = uneAutreClio ;
+        reno.nbVoitures = 1 ;
+        uneAutreClio.proprietaire = reno ;
+        
+        reno.liste_voitures[1] = uneMicra ;
+        reno.nbVoitures = 2 ;
+        uneMicra.proprietaire = reno ;
+        
+        System.out.println(bob +"\n"+ reno);
+        
+        bob.ajouter_voiture(uneMicra);
+        reno.ajouter_voiture(uneGolf);
         
     }
     
