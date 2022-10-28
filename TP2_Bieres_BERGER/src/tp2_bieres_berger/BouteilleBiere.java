d6b941bb8539a17600f3bbbb26ca097e4fc7c3e1
Thomas Berger <thomas.berger@epfedu.fr>
2022-10-28 14:33:37 +0200

    TP2 - Partie 1 Création de la classe BouteilleBiere Création de 5 bières grâce au constructeur

---
+++ TP2_Bieres_BERGER/src/tp2_bieres_berger/BouteilleBiere.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_bieres_berger;
+
+/**
+ *
+ * @author berge
+ */
+public class BouteilleBiere {
+    String nom;
+    double degreAlcool;
+    String brasserie;
+    boolean ouverte;
+
+    public void lireEtiquette(){
+        System.out.println("Bouteille de " + nom +" (" + degreAlcool +" degres) \nBrasserie : " + brasserie ) ;
+    }
+    public BouteilleBiere(String unNom, double unDegre, String uneBrasserie){
+        nom = unNom;
+        degreAlcool = unDegre;
+        brasserie = uneBrasserie;
+        ouverte = false;
+    }
+}
+
--- TP2_Bieres_BERGER/src/tp2_bieres_berger/TP2_Bieres_BERGER.java
+++ TP2_Bieres_BERGER/src/tp2_bieres_berger/TP2_Bieres_BERGER.java
@@ -14,7 +14,22 @@ public class TP2_Bieres_BERGER {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls",7.0,"Dubuisson");
+        uneBiere.lireEtiquette();
+       
+        BouteilleBiere sBiere = new BouteilleBiere("Leffe",6.6,"Abbaye de Leffe");
+        sBiere.lireEtiquette();
+        
+        BouteilleBiere tBiere = new BouteilleBiere("Pelfort",6.5,"Pelfort");
+        tBiere.lireEtiquette();
+        
+        BouteilleBiere qBiere = new BouteilleBiere("Heineken",5,"Heineken");
+        qBiere.lireEtiquette();
+        
+        BouteilleBiere cBiere = new BouteilleBiere("Guinness",4.2,"St. James's Gate");
+        cBiere.lireEtiquette();
+        
     }
     
 }
