c1c507040cb2ac20901c5138bd448ade49cd92db
Thomas Berger <thomas.berger@epfedu.fr>
2022-10-28 14:55:12 +0200

    TP2 - Partie 1 Fin

--- TP2_Bieres_BERGER/src/tp2_bieres_berger/BouteilleBiere.java
+++ TP2_Bieres_BERGER/src/tp2_bieres_berger/BouteilleBiere.java
@@ -12,16 +12,36 @@ public class BouteilleBiere {
     String nom;
     double degreAlcool;
     String brasserie;
-    boolean ouverte;
+    boolean ouverte = false;
 
     public void lireEtiquette(){
         System.out.println("Bouteille de " + nom +" (" + degreAlcool +" degres) \nBrasserie : " + brasserie ) ;
     }
+    
     public BouteilleBiere(String unNom, double unDegre, String uneBrasserie){
         nom = unNom;
         degreAlcool = unDegre;
         brasserie = uneBrasserie;
         ouverte = false;
     }
+    
+    public boolean Décapsuler(){
+       if (ouverte == false){  
+           ouverte = true;
+           return true;
+       }else{
+           return false;           
+       } 
+       }
+    
+    @Override
+    public String toString(){
+        String chaine_a_retourner;
+        chaine_a_retourner = nom + " (" + degreAlcool + " degrés) Ouverte ? "; 
+        if (ouverte == true ) chaine_a_retourner += "oui" ;
+        else chaine_a_retourner += "non" ;
+        return chaine_a_retourner ;
+    }
+
 }
 
